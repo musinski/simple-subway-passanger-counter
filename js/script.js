6e71edec0e783caeda30b0a5d6ec35f2cc370745
HerboSpringston <herbospringston@gmail.com>
2021-07-01 11:19:42 -0500

    Added save functionality

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-//document.getElementById("count-el").innerText = 10;
 let passangerCount = 0;
+let saveElement = document.getElementById("saved-count");
 
 function increment() {
     passangerCount += 1;
     document.getElementById("count-el").innerText = passangerCount;
+}
+
+function save() {
+    let countString = passangerCount;
+    let dashSeparator = " - ";
+    saveElement.textContent += countString + dashSeparator;
 }
 
 function resetCounter() {
@@ -13,6 +19,7 @@ function resetCounter() {
     if (userResponse == 'yes' || userResponse == 'y') {
         passangerCount = 0;
         document.getElementById("count-el").innerText = passangerCount;
+        document.getElementById("saved-count").innerHTML = "";
     } else if (userResponse == 'no' || userResponse == 'n'){
         alert("Not resetting.");
     }
